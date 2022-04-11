08082fc58a321501077767324ab6119ed5819ba5
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2022-04-11 03:41:44 -0700

    Complete rework7

--- server.js
+++ server.js
@@ -54,12 +54,11 @@ app.get('/login', (req, res) => {
 
 // After login, user is sent to callback route
 app.get('/callback', (req, res) => {
-  
+ 
   const error = req.query.error;
   const code = req.query.code;
   const state = req.query.state;
-  var user = "";
-
+ 
   if (error) {
     console.error('Callback Error:', error);
     res.send(`Callback Error: ${error}`);
@@ -72,14 +71,13 @@ app.get('/callback', (req, res) => {
       const access_token = data.body['access_token'];
       const refresh_token = data.body['refresh_token'];
       const expires_in = data.body['expires_in'];
-      
        
       spotifyApi.setAccessToken(access_token);
       spotifyApi.setRefreshToken(refresh_token);
-
+ 
       console.log('access_token:', access_token);
       console.log('refresh_token:', refresh_token);
-
+ 
       console.log(
         `Sucessfully retreived access token. Expires in ${expires_in} s.`
       );
@@ -87,21 +85,23 @@ app.get('/callback', (req, res) => {
       setInterval(async () => {
         const data = await spotifyApi.refreshAccessToken();
         const access_token = data.body['access_token'];
-
+ 
         console.log('The access token has been refreshed!');
         console.log('access_token:', access_token);
         spotifyApi.setAccessToken(access_token);
       }, expires_in / 2 * 1000);
-
-     
-    const string = encodeURIComponent('success')
-     res.redirect('https://www.plotterfy.com/?valid=' + string
+ 
+    
+      const string = encodeURIComponent('success')
+      res.redirect('https://www.plotterfy.com/?valid=' + string)
+      
+      
     })
     .catch(error => {
       console.error('Error getting Tokens:', error);
       res.send(`Error getting Tokens: ${error}`);
     });
-});
+ });
 
 app.get('/topsongs', (req, res) => {
   console.log("topsongs: 1")
